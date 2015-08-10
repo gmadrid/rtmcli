97acfeb17f2aa6eecc48f99c43a64ce0c6639552
George Madrid <gmadrid@gmail.com>
2015-08-10 13:30:18 -0400

    Moving magic strings to constants like they tell you in computer class.

--- rtmapi.hs
+++ rtmapi.hs
@@ -23,21 +23,39 @@ data RtmConfig = RtmConfig {
   } deriving (Read, Show)
 
 authBaseUrl = "https://www.rememberthemilk.com/services/auth/"
-baseUrl = "https://api.rememberthemilk.com/services/rest/" :: ByteString
+baseUrl     = "https://api.rememberthemilk.com/services/rest/"
+
+methodGetFrob  = "rtm.auth.getFrob"
+methodGetToken = "rtm.auth.getToken"
+
+apiKeyParam      = "api_key"
+apiSigParam      = "api_sig"
+formatParam      = "format"
+formatJsonValue  = "json"
+frobParam        = "frob"
+methodParam      = "method"
+permsParam       = "perms"
+permsDeleteValue = "delete"
+
+authJsonKey  = "auth"
+frobJsonKey  = "frob"
+permsJsonKey = "perms"
+rspJsonKey   = "rsp"
+tokenJsonKey = "token"
 
 methodUrl :: RtmConfig -> ByteString -> [ (ByteString, ByteString) ] -> ByteString
-methodUrl r m ps = buildUrl r baseUrl $ [ ("method", m),
-                                          ("format", "json") ] ++ ps
+methodUrl r m ps = buildUrl r baseUrl $ [ (methodParam, m),
+                                          (formatParam, formatJsonValue) ] ++ ps
 
 authUrl :: RtmConfig -> ByteString -> ByteString
-authUrl r f = buildUrl r authBaseUrl [ ("perms", "delete"),
-                                       ("frob", f) ]
+authUrl r f = buildUrl r authBaseUrl [ (permsParam, permsDeleteValue),
+                                       (frobParam, f) ]
 
 buildUrl :: RtmConfig -> ByteString -> [ (ByteString, ByteString) ] -> ByteString
 buildUrl r b ps = b ++ q
-  where ps' = ("api_key", apiKey r) : ps
+  where ps' = (apiKeyParam, apiKey r) : ps
         sig = signParams (secret r) ps'
-        q = renderSimpleQuery True $ ("api_sig", sig) : ps'
+        q = renderSimpleQuery True $ (apiSigParam, sig) : ps'
 
 callMethod :: FromJSON r => RtmConfig -> Manager -> ByteString -> [ (ByteString, ByteString) ] ->
               IO (Either ByteString r)
@@ -54,62 +72,39 @@ callMethod rc mgr m ps = do
 
 getFrob :: RtmConfig -> Manager -> IO (Either ByteString ByteString)
 getFrob rc mgr = do
-  efr <- callMethod rc mgr "rtm.auth.getFrob" []
+  efr <- callMethod rc mgr methodGetFrob []
   case efr of
    Right fr -> return . Right . fromString $ frob fr
    Left e -> return $ Left e
 
 
-getFrob' :: RtmConfig -> Manager -> IO (Either ByteString ByteString)
-getFrob' r mgr = do
-  let u = methodUrl r "rtm.auth.getFrob" []
-  req <- parseUrl $ C8.unpack u
-  withResponse req mgr readResponse
-    where readResponse rsp = do
-            c <- brRead . responseBody $ rsp
-            let mf = decode $ fromStrict c
-            return $ maybe (Left "Decode failed in getFrob")
-              (\f -> Right $ fromString $ frob f) mf
-
 getToken :: RtmConfig -> Manager -> ByteString -> IO (Either ByteString ByteString)
 getToken rc mgr f = do
-  etk <- callMethod rc mgr "rtm.auth.getToken" [ ("frob", f) ]
+  etk <- callMethod rc mgr methodGetToken [ (frobParam, f) ]
   case etk of
    Right tk -> return . Right . fromString $ tokenToken tk
    Left e -> return $ Left e
 
 
-getToken' :: RtmConfig -> Manager -> ByteString -> IO (Either ByteString ByteString)
-getToken' r mgr f = do
-  let u = methodUrl r "rtm.auth.getToken" [ ("frob", f) ]
-  req <- parseUrl $ C8.unpack u
-  withResponse req mgr readResponse
-    where readResponse rsp = do
-            c <- brRead . responseBody $ rsp
-            let mt = decode $ fromStrict c
-            return $ maybe (Left "Decode failed in getToken")
-              (\t -> Right $ fromString $ tokenToken t) mt
-
-
-twoDigitHex :: Word8 -> String
-twoDigitHex w = drop ((length s) - 2) s
-  where s = "00" ++ (showHex w "")
-
 signParams :: ByteString -> [ ( ByteString, ByteString ) ] -> ByteString
 signParams secret ps = fromString dsp
   where cps = concat . (secret:) . map (\(a,b) -> a ++ b) $ sort ps
         hsh = Crypto.Hash.MD5.hash cps
-        dsp = foldr (\w acc -> (twoDigitHex w) ++ acc) "" hsh
+        dsp = foldr (\w acc -> (twoDigitHex w) ++ acc) mempty hsh
+        twoDigitHex w = drop ((length s) - 2) s
+          where s = "00" ++ (showHex w mempty)
+        
 
 
 data Frob = Frob { frob :: String }
             deriving (Show)
 
 instance FromJSON Frob where
-  parseJSON (Object v) = Frob <$> (rsp >>= (.: "frob"))
-    where rsp = (v .: "rsp")
+  parseJSON (Object v) = Frob <$> (rsp >>= (.: frobJsonKey))
+    where rsp = (v .: rspJsonKey)
   parseJSON _ = mzero
   
+
 data TokenResp = TokenResp {
   tokenToken :: String,
   tokenPerms :: String
@@ -117,8 +112,8 @@ data TokenResp = TokenResp {
 
 instance FromJSON TokenResp where
   parseJSON (Object v) = TokenResp <$>
-                         (auth >>= (.: "token")) <*>
-                         (auth >>= (.: "perms"))
-    where rsp = (v .: "rsp")
-          auth = (rsp >>= (.: "auth"))
+                         (auth >>= (.: tokenJsonKey)) <*>
+                         (auth >>= (.: permsJsonKey))
+    where rsp = (v .: rspJsonKey)
+          auth = (rsp >>= (.: authJsonKey))
   parseJSON _ = mzero
